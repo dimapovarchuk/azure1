16d7fcff336d5f3ba05a4e90940a5a5b2b072110
dmytro_povarchuk <Dmytro_Povarchuk@epam.com>
2024-10-07 14:09:44 +0000

    Fixed

--- task03/main.tf
+++ task03/main.tf
@@ -2,19 +2,26 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_resource_group" "existing" {
+  name = var.rg_name
+}
+
 resource "azurerm_resource_group" "this" {
-  name     = var.rg_name
-  location = var.location
+  name     = data.azurerm_resource_group.existing.name
+  location = data.azurerm_resource_group.existing.location
   tags     = var.tags
 }
 
 resource "azurerm_storage_account" "this" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.this.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = var.storage_account_replication_type
-  tags                     = var.tags
+  name                            = var.storage_account_name
+  resource_group_name             = azurerm_resource_group.this.name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = var.storage_account_replication_type
+  https_traffic_only_enabled      = true
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+  tags                            = var.tags
 }
 
 resource "azurerm_virtual_network" "this" {
