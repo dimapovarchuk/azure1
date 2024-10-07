71d3f61f8cda5ea38cecdcccac55ce58272b15b6
dmytro_povarchuk <Dmytro_Povarchuk@epam.com>
2024-10-07 14:27:23 +0000

    Fixed

--- task03/main.tf
+++ task03/main.tf
@@ -13,7 +13,8 @@ resource "azurerm_resource_group" "this" {
 }
 
 data "azurerm_storage_account" "existing" {
-  name = var.storage_account_name
+  name                = var.storage_account_name
+  resource_group_name = azurerm_resource_group.this.name
 }
 
 resource "azurerm_virtual_network" "this" {
--- task03/outputs.tf
+++ task03/outputs.tf
@@ -5,7 +5,7 @@ output "rg_id" {
 
 output "sa_blob_endpoint" {
   description = "The blob endpoint URL of storage account"
-  value       = azurerm_storage_account.this.primary_blob_endpoint
+  value       = data.azurerm_storage_account.existing.primary_blob_endpoint
 }
 
 output "vnet_id" {
